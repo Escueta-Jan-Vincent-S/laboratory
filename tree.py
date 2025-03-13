c8e6107eab0069a27699860cea35dab5b56b0dd3
Escueta, Jan Vincent S. <escuetajanvincent04@gmail.com>
2025-03-13 10:26:17 +0800

    first commit

--- tree.py
+++ tree.py
@@ -25,6 +25,15 @@ def postorder_traversal(root):
         postorder_traversal(root.right)
         print(root.value, end=' ')
 
+def preorder_traversal_swap(root):
+    if root:
+        root.left, root.right = root.right, root.left
+        root.left.left, root.left.right = root.left.right, root.left.left
+        root.right.left, root.right.right = root.right.right, root.right.left
+        print(root.value, end=' ')
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
@@ -33,7 +42,8 @@ root.left.right = TreeNode(5)
 root.right.left = TreeNode(6)
 root.right.right = TreeNode(7)
 
-print("In-order traversal")
+print("Original")
+print("\nIn-order traversal")
 inorder_traversal(root)
 
 print("\nPre-order traversal")
@@ -41,3 +51,8 @@ preorder_traversal(root)
 
 print("\nPost-order traversal")
 postorder_traversal(root)
+print("\n")
+
+print("\nAfter Swap")
+print("\nPre-order traversal")
+preorder_traversal_swap(root)
